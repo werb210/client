9ad29501d0b01cc53d0a3e1e6c9a19c8181ff1f6
werb210 <todd.w@boreal.financial>
2025-10-25 10:10:39 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./client/index.html",
-    "./client/src/**/*.{js,ts,jsx,tsx}",
-    "./client/src/pages/**/*.{ts,tsx,jsx,js}",
-    "./client/src/v2-design-system/**/*.{ts,tsx,jsx,js}",
-    "./client/src/components/**/*.{ts,tsx,jsx,js}",
-    "./client/src/routes/**/*.{ts,tsx,jsx,js}",
+    "./client/src/**/*.{js,ts,jsx,tsx}"
   ],
-  darkMode: ["class"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
