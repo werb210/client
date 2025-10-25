54d3c70346944f8236e260ee2aa9e5acb970441b
werb210 <todd.w@boreal.financial>
2025-10-25 10:30:30 -0600

    Create postcss.config.cjs

---
+++ client/postcss.config.cjs
@@ -0,0 +1,9 @@
+/** PostCSS config for Tailwind v4 + HSL vars */
+export default {
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": "postcss-nesting",
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
