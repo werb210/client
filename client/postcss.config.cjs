4987a6fbca08831e87cdb9c6ad39d22bb80fd02d
werb210 <todd.w@boreal.financial>
2025-10-25 10:32:01 -0600

    Update postcss.config.cjs

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,8 +1,8 @@
-/** PostCSS config for Tailwind v4 + HSL vars */
-export default {
+/** PostCSS config for Tailwind v4 + HSL vars (CommonJS) */
+module.exports = {
   plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": "postcss-nesting",
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
     autoprefixer: {},
   },
