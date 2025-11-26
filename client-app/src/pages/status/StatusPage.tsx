6a7996445ea806eed455d594f8759222a39af375
werb210 <todd.w@boreal.financial>
2025-11-26 16:14:26 -0700

    Remove client folder

--- client-app/src/pages/status/StatusPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function StatusPage() {
-  return <div>Status page</div>;
-}
--- client-app/src/types/index.ts
+++
@@ -1,1 +0,0 @@
-// Shared DTO and interface definitions will be added here as the client evolves.
