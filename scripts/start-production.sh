c5928054afa75ff284c639c846912316bc55359e
werb210 <todd.w@boreal.financial>
2025-11-26 17:04:16 -0700

    Delete scripts directory

--- scripts/start-production.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Production startup script
-# Forces NODE_ENV=production for true production mode
-
-echo "🚀 Starting application in PRODUCTION mode..."
-NODE_ENV=production tsx server/index.ts
