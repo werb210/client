50b8f2320aa560d72314c348a38963fce279272f
werb210 <todd.w@boreal.financial>
2025-11-26 17:14:51 -0700

    Delete public directory

--- public/service-worker.js
+++
@@ -1,4 +0,0 @@
-/* Dev-safe SW (no caching) */
-self.addEventListener("install", e => self.skipWaiting());
-self.addEventListener("activate", e => clients.claim());
-self.addEventListener("fetch", () => {});  // no cache.addAll / add
