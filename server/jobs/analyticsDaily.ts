ab872687bae28b567e23cde02dfc4f0f48c96b31
werb210 <todd.w@boreal.financial>
2025-11-26 17:12:06 -0700

    Delete server directory

--- server/jobs/analyticsDaily.ts
+++
@@ -1,9 +0,0 @@
-import { computeDaily } from "../services/analytics";
-
-export async function backfillAnalytics(daysBack = 90) {
-  const now = new Date();
-  for (let i=daysBack; i>=0; i--){
-    const d = new Date(now.getTime() - i*24*3600*1000);
-    await computeDaily(d.toISOString().slice(0,10));
-  }
-}
