3bfc8357fcbd62b85ef550bfd6dae73427545a0c
werb210 <todd.w@boreal.financial>
2025-11-26 19:00:38 -0700

    Add layout shell, global styles, store, and API client

--- client-app/src/app.tsx
+++ client-app/src/app.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Router from "./router";
+import Layout from "./layout/Layout";
 
 const App: React.FC = () => {
-  return <Router />;
+  return (
+    <Layout>
+      <Router />
+    </Layout>
+  );
 };
 
 export default App;
