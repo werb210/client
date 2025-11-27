c1578236e0155822a9d4e4f04c78d980d388bbce
werb210 <todd.w@boreal.financial>
2025-11-26 19:14:50 -0700

    Add base layout and routing shells

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,22 @@
-import "./styles/reset.css";
-import "./styles/global.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AppLayout from "./layout/AppLayout";
+import Home from "./pages/Home";
+import StartApplication from "./pages/StartApplication";
+import NotFound from "./pages/NotFound";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apply" element={<StartApplication />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   </React.StrictMode>
 );
