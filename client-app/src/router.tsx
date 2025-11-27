3d2f36df1bc81332ea310d938d0da39bbb9ffba4
werb210 <todd.w@boreal.financial>
2025-11-26 19:06:43 -0700

    Add initial layout components

--- client-app/src/router.tsx
+++ client-app/src/router.tsx
@@ -3,15 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Apply from "./pages/Apply";
 import Status from "./pages/Status";
+import AppLayout from "./layout/AppLayout";
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apply" element={<Apply />} />
-        <Route path="/status" element={<Status />} />
-      </Routes>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apply" element={<Apply />} />
+          <Route path="/status" element={<Status />} />
+        </Routes>
+      </AppLayout>
     </BrowserRouter>
   );
 };
