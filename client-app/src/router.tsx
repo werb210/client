1e89be795828ae26ce44de1cbc23479f3ae296a0
werb210 <todd.w@boreal.financial>
2025-11-26 17:23:47 -0700

    Create client app scaffold

---
+++ client-app/src/router.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Client App Running</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
