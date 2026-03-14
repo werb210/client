4bdbbc5e63c194a6b2ee817ec1a8ce1466e4c3fb
werb210 <todd.w@boreal.financial>
2026-03-14 11:34:25 -0600

    Stabilize client router entrypoint and global error boundary

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 );
