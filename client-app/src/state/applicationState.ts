8c21928e65ebe20e90d5b47e959d13f51c55b817
werb210 <todd.w@boreal.financial>
2026-03-14 10:34:06 -0600

    Add initial application step UI and service layer

---
+++ client-app/src/state/applicationState.ts
@@ -0,0 +1,9 @@
+export interface ApplicationState {
+  companyName?: string;
+  contactName?: string;
+  email?: string;
+  phone?: string;
+  amount?: number;
+}
+
+export const initialApplicationState: ApplicationState = {};
